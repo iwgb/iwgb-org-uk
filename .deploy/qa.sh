b52eb4301a2391dfdb6dcdd4e6a6f853e954b4e8
Guy Mac <hello@guymac.eu>
2020-04-04 17:10:07 +0100

    Workflow changes

--- .deploy/qa.sh
+++ .deploy/qa.sh
@@ -1,24 +1,21 @@
 #!/bin/bash
-cd /var/repo/iwgb-org-uk-qa
+cd /var/repo/iwgb-org-uk-qa || exit 1
 
 rsync -a . /var/www/iwgb-org-uk-qa --delete --exclude .git --exclude .deploy --exclude .github --exclude vendor --exclude .gitignore
 
-cd /var/www/iwgb-org-uk-qa/public
+cd /var/www/iwgb-org-uk-qa/public || exit 1
 mv index.php index.temp.php
 mv maintenance.php index.php
 
-cd /var/repo/iwgb-org-uk-static
+cd /var/repo/iwgb-org-uk-static || exit 1
 rsync -a . /var/www/iwgb-org-uk-qa
 
-cd /var/www/iwgb-org-uk-qa
-export COMPOSER_HOME=/usr/local/bin
-composer install
-composer update
+runuser -l deploy -c 'cd /var/www/iwgb-org-uk-qa && composer install'
 chmod -R 777 var
 
-cd /var/www/iwgb-org-uk-qa/assets/css
+cd /var/www/iwgb-org-uk-qa/assets/css || exit 1
 sass --update --no-cache --style compressed .:.
 
-cd /var/www/iwgb-org-uk-qa/public
+cd /var/www/iwgb-org-uk-qa/public || exit 1
 mv index.php maintenance.php
 mv index.temp.php index.php
