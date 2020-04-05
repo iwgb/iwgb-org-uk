651c47cc7b685b95735bfeac2d871616c754ee9a
Guy Mac <hello@guymac.eu>
2020-04-05 23:27:07 +0100

    fix(cd): deploy holding page scaling fix

--- public/maintenance.php
+++ public/maintenance.php
@@ -8,6 +8,7 @@ echo <<<'HTML'
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <title>Loading... &middot; IWGB</title>
     <meta http-equiv="refresh" content="5">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css">
